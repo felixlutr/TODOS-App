558060fcd1be937aaf78ad253a02b8e58112c3c3
Felix Lutrovnik <felixl@ictbit.com>
2024-06-08 20:09:07 +0300

    refinned some css and added date to tasks.

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { FaCheck, FaPencil, FaTrash } from "react-icons/fa6";
 
+
+const date = new Date().toLocaleDateString()
+
+
 const Todo = ({
   id,
   isChecked,
@@ -24,7 +28,7 @@ const Todo = ({
         checked={isChecked}
         onChange={() => toggleTodoCheckedStatus(id)}
         className={"todo-checkbox"}
-        readOnly // this checkbox does nothing, the parrent control the checked state.
+        readOnly 
       />
       {isEditMode ? (
         <textarea
@@ -38,7 +42,7 @@ const Todo = ({
           }}
         />
       ) : (
-        <span>{name}</span>
+        <span>{name}, {date}</span>
       )}
       {isEditMode ? (
         <FaCheck
